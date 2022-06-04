105251e549135b7b9583fe42aeaf4e92e537fe43
Dulon18 <dulon587@gmail.com>
2022-06-04 13:56:36 +0600

    display doctort in home page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Doctor;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function user()
     {
-        return view('user.home');
+        if(Auth::id())
+        {
+            return redirect('home');
+        }
+        else{
+
+            $doctors=Doctor::all();
+            //dd($doctor);
+            return view('user.home',compact('doctors'));
+
+        }
+       
     }
 
     public function admin()
--- resources/views/user/doctor.blade.php
+++ resources/views/user/doctor.blade.php
@@ -1,83 +1,32 @@
+
 <div class="page-section">
     <div class="container">
       <h1 class="text-center mb-5 wow fadeInUp">Our Doctors</h1>
 
       <div class="owl-carousel wow fadeInUp" id="doctorSlideshow">
+        
+      @foreach($doctors as $d)
         <div class="item">
           <div class="card-doctor">
+  
             <div class="header">
-              <img src="{{url('frontend/assets/img/doctors/doctor_1.jpg')}}" alt="">
+            <img src="{{url('/storage/'.$d->image)}}" alt="Image not found">
               <div class="meta">
                 <a href="#"><span class="mai-call"></span></a>
                 <a href="#"><span class="mai-logo-whatsapp"></span></a>
               </div>
             </div>
             <div class="body">
-              <p class="text-xl mb-0">Dr. Stein Albert</p>
-              <span class="text-sm text-grey">Cardiology</span>
+              <p class="text-xl mb-0">{{$d->name}}</p>
+              <span class="text-sm text-grey">{{$d->specility}}</span>
             </div>
+          
           </div>
         </div>
-        <div class="item">
-          <div class="card-doctor">
-            <div class="header">
-              <img src="{{url('frontend/assets/img/doctors/doctor_2.jpg')}}" alt="">
-              <div class="meta">
-                <a href="#"><span class="mai-call"></span></a>
-                <a href="#"><span class="mai-logo-whatsapp"></span></a>
-              </div>
-            </div>
-            <div class="body">
-              <p class="text-xl mb-0">Dr. Alexa Melvin</p>
-              <span class="text-sm text-grey">Dental</span>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <div class="card-doctor">
-            <div class="header">
-              <img src="{{url('frontend/assets/img/doctors/doctor_3.jpg')}}" alt="">
-              <div class="meta">
-                <a href="#"><span class="mai-call"></span></a>
-                <a href="#"><span class="mai-logo-whatsapp"></span></a>
-              </div>
-            </div>
-            <div class="body">
-              <p class="text-xl mb-0">Dr. Rebecca Steffany</p>
-              <span class="text-sm text-grey">General Health</span>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <div class="card-doctor">
-            <div class="header">
-              <img src="{{url('frontend/assets/img/doctors/doctor_3.jpg')}}" alt="">
-              <div class="meta">
-                <a href="#"><span class="mai-call"></span></a>
-                <a href="#"><span class="mai-logo-whatsapp"></span></a>
-              </div>
-            </div>
-            <div class="body">
-              <p class="text-xl mb-0">Dr. Rebecca Steffany</p>
-              <span class="text-sm text-grey">General Health</span>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <div class="card-doctor">
-            <div class="header">
-              <img src="{{url('frontend/assets/img/doctors/doctor_3.jpg')}}" alt="">
-              <div class="meta">
-                <a href="#"><span class="mai-call"></span></a>
-                <a href="#"><span class="mai-logo-whatsapp"></span></a>
-              </div>
-            </div>
-            <div class="body">
-              <p class="text-xl mb-0">Dr. Rebecca Steffany</p>
-              <span class="text-sm text-grey">General Health</span>
-            </div>
-          </div>
-        </div>
+        @endforeach
+      
+        
+        
       </div>
     </div>
   </div>
